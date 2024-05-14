9d96ff3b8ae49bf6e6fa14d555e32c67ad876dd7
Junoiri <martasadlowska77@gmail.com>
2024-05-14 21:51:40 +0200

    Updated with #6 tutorial content

---
+++ src/menu-app-bar/MenuAppBar.tsx
@@ -0,0 +1,40 @@
+import { AppBar, IconButton, Toolbar, Typography, Box } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import { useNavigate } from 'react-router-dom';
+
+export default function MenuAppBar() {
+  const navigate = useNavigate();
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Library
+        </Typography>
+        <Box>
+          <IconButton
+            size="large"
+            edge="end"
+            color="inherit"
+            aria-label="account of current user"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={() => navigate('/login')}
+            sx={{ ml: 2 }}
+          >
+            <AccountCircle />
+          </IconButton>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+}
